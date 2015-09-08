0809d993560b88d48aabec7ac0832a42bcad2b26
CM Lubinski <christopher.lubinski@gsa.gov>
2015-09-08 23:12:12 +0000

    Turn off debugging

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=$VCAP_APP_PORT atf_eregs.wsgi:application
+web: django-admin.py collectstatic --settings=atf_eregs.settings --noinput && waitress-serve --port=$VCAP_APP_PORT atf_eregs.wsgi:application
--- atf_eregs/settings.py
+++ atf_eregs/settings.py
@@ -1,3 +1,12 @@
+import json
+import os
+
 from regulations.settings.base import *
 
 INSTALLED_APPS = ('atf_eregs',) + INSTALLED_APPS
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+vcap = json.loads(os.environ.get('VCAP_APPLICATION', '{}'))
+ALLOWED_HOSTS = vcap.get('application_uris', [])
