72b10fca8b6df3591ad0b2636257519ca9ef2e1b
CM Lubinski <christopher.lubinski@gsa.gov>
2015-12-20 22:27:27 +0000

    Configure ATF layers
    Removes SxS and interpretations, which aren't present in ATF regs

--- atf_eregs/settings/base.py
+++ atf_eregs/settings/base.py
@@ -18,3 +18,19 @@ API_BASE = 'http://localhost:{}/api/'.format(
     os.environ.get('VCAP_APP_PORT', '8000'))
 
 STATICFILES_DIRS = ['compiled']
+
+DATA_LAYERS = (
+    'regulations.generator.layers.defined.DefinedLayer',
+    'regulations.generator.layers.definitions.DefinitionsLayer',
+    'regulations.generator.layers.formatting.FormattingLayer',
+    'regulations.generator.layers.internal_citation.InternalCitationLayer',
+    'regulations.generator.layers.key_terms.KeyTermsLayer',
+    'regulations.generator.layers.meta.MetaLayer',
+    'regulations.generator.layers.paragraph_markers.ParagraphMarkersLayer',
+    'regulations.generator.layers.toc_applier.TableOfContentsLayer',
+    'regulations.generator.layers.graphics.GraphicsLayer',
+)
+
+SIDEBARS = (
+    'regulations.generator.sidebar.help.Help',
+)
