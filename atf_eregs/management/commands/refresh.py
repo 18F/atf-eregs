57fcb9eedae5e403b1f066a03c829e00100c926b
Joshua Carp <jm.carp@gmail.com>
2016-02-03 15:19:09 -0500

    Migrate and refresh on 0th instance only.
    Add a new management command that runs migration and rebuilds the search
    index if not running on cloud.gov or running on the 0th instance on
    cloud.gov.
    
    Related to #199.

---
+++ atf_eregs/management/commands/refresh.py
@@ -0,0 +1,13 @@
+from django.core import management
+from django.core.management.base import BaseCommand
+
+from cfenv import AppEnv
+
+class Command(BaseCommand):
+    help = 'Migrate database and rebuild search index on cloud.gov'
+
+    def handle(self, *args, **options):
+        env = AppEnv()
+        if env.index is None or env.index == 0:
+            management.call_command('migrate', fake_initial=True)
+            management.call_command('rebuild_index', interactive=False, remove=True)
