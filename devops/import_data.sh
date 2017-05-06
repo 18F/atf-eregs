b2ef76b6a148200697415d17b13043f6aa614011
CM Lubinski <christopher.lubinski@gsa.gov>
2017-05-06 15:04:24 -0400

    Add second docker setup for parser.
    The parser and main ATF app use incompatible versions of Django, so let's
    create a very similar, but separate setup for it. This also includes an
    import_data.sh script to get the data setup.

---
+++ devops/import_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -v -e
+
+docker-compose up -d dev-with-db
+
+# wait for api to startup
+until (curl http://localhost:9001/api/notice &>/dev/null)
+do
+  echo "Startup: Waiting for API"
+  sleep 1
+done
+
+cd eregs_extensions
+PARSE="docker-compose -f docker-compose.yml -f atf-network.yml run --rm eregs"
+
+$PARSE pipeline 27 447 http://dev-with-db:8001/api
+$PARSE pipeline 27 478 http://dev-with-db:8001/api
+$PARSE pipeline 27 479 http://dev-with-db:8001/api
+$PARSE pipeline 27 555 http://dev-with-db:8001/api
+$PARSE pipeline 27 646 http://dev-with-db:8001/api
