9f8faef514b51974341702e6a7d376cdc97573d3
CM Lubinski <christopher.lubinski@gsa.gov>
2017-08-11 14:58:41 -0400

    Add a "Related documents" sidebar widget.
    This adds an (empty) sidebar above the existing Rulings sidebar (it'll
    eventually replace the latter). This includes some dummy data that can be
    turned on by commenting out the first `return` in the ATFResourcess class.

--- atf_eregs/sidebar.py
+++ atf_eregs/sidebar.py
@@ -21,3 +21,32 @@ class Rulings(SidebarBase):
         rulings = sorted(rulings.values(), key=lambda r: r['id'],
                          reverse=True)
         return {'rulings': rulings}
+
+
+class ATFResources(SidebarBase):
+    shorthand = 'atf-resources'
+
+    def context(self, http_client, request):
+        return {'count': 0, 'groups': []}
+        rulings = [
+            {'id': '2010-2', 'title': 'Some Example',
+             'url': 'http://example.com'},
+            {'id': '2010-1', 'title': 'Another Example',
+             'url': 'http://example.com'},
+        ]
+        forms = [
+            {'id': 'Form 123', 'title': 'Registering Stuff',
+             'url': 'http://example.com'},
+            {'id': 'Form 345', 'title': 'More Stuff',
+             'url': 'http://example.com'},
+        ]
+
+        # Example data
+        return {
+            'count': len(rulings) + len(forms),
+            'groups': [{
+                'name': 'Ruling', 'count': len(rulings), 'entries': rulings
+            }, {
+                'name': 'Form', 'count': len(forms), 'entries': forms
+            }],
+        }
