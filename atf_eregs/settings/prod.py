e6a0f95318181e9c13f1661097fa170f8a50e24d
CM Lubinski <christopher.lubinski@gsa.gov>
2017-09-01 09:43:15 -0400

    Set an upper bound on caches.
    We run the synchronization script for grabbing data from ATF once an hour. If
    we then invalidate every hour, we're at a maximum delay of 2 hours.

--- atf_eregs/settings/prod.py
+++ atf_eregs/settings/prod.py
@@ -3,7 +3,17 @@ import re
 from cfenv import AppEnv
 
 from .base import *  # noqa
-from .base import HAYSTACK_CONNECTIONS  # explicitly referenced below
+from .base import CACHES, HAYSTACK_CONNECTIONS  # explicitly referenced below
+
+def _limit_caches(max_timeout):
+    """While the regulation content doesn't change often, we want to allow ATF
+    to update their "Related Documents" relatively quickly."""
+    for cache in CACHES.values():
+        current_timeout = cache.get('TIMEOUT', 300)     # Django's default
+        cache['TIMEOUT'] = min(current_timeout, max_timeout)
+
+_limit_caches(60*60)    # 1 hour
+
 
 DEBUG = False
 TEMPLATE_DEBUG = False
@@ -24,6 +34,7 @@ HTTP_AUTH_USER = env.get_credential('HTTP_AUTH_USER')
 HTTP_AUTH_PASSWORD = env.get_credential('HTTP_AUTH_PASSWORD')
 
 ALLOWED_HOSTS = ['localhost'] + env.uris
+
 
 try:
     from local_settings import *    # noqa
